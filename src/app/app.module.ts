92de9d620503776706a78eb89ff3dd8f52ad5ec9
George Allen <george.allen93@outlook.com>
2016-11-08 20:16:31 +0000

    added bootstrap and font awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { GrgalnRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
@@ -23,6 +24,7 @@ const myFirebaseAuthConfig = {
     HttpModule,
     GrgalnRoutingModule,
     CoreModule,
+    NgbModule.forRoot(),
     AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [
