ad7e832c3b3ff88160e6f9086b53b7023e273e5c
George Allen <george.allen93@outlook.com>
2016-11-08 20:03:40 +0000

    updated angular cli

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,7 +1,6 @@
 // Typings reference file, see links for more information
 // https://github.com/typings/typings
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
-
 declare var System: any;
-declare var require: any;
-declare var module: any;
+declare var require: NodeRequire;
+declare var module: NodeModule;
