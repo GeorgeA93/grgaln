1cd6c598cf7d962deeb386d7fcc7269343e03ff3
George Allen <george.allen93@outlook.com>
2016-11-08 22:47:27 +0000

    started bootstrap implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,31 +4,33 @@ import { HttpModule } from '@angular/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { GrgalnRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
 import { HomeComponent } from './home/home.component';
 import { CoreModule } from './core/core.module';
 import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
 import { firebaseConfig } from '../sensitive/firebase.config';
 
 const myFirebaseAuthConfig = {
-  provider: AuthProviders.Google,
-  method: AuthMethods.Redirect
+    provider: AuthProviders.Google,
+    method: AuthMethods.Redirect
 }
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpModule,
-    GrgalnRoutingModule,
-    CoreModule,
-    NgbModule.forRoot(),
-    AngularFireModule.initializeApp(firebaseConfig)
-  ],
-  providers: [
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpModule,
+        SharedModule,
+        GrgalnRoutingModule,
+        CoreModule,
+        NgbModule.forRoot(),
+        AngularFireModule.initializeApp(firebaseConfig)
+    ],
+    providers: [
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectsComponent } from './projects.component';
 import { ProjectsListComponent } from './projects-list/projects-list.component';
 import { ProjectItemComponent } from './project-item/project-item.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    ProjectsRoutingModule
-  ],
-  declarations: [ProjectsComponent, ProjectsListComponent, ProjectItemComponent]
+    imports: [
+        CommonModule,
+        ProjectsRoutingModule,
+        SharedModule
+    ],
+    declarations: [ProjectsComponent, ProjectsListComponent, ProjectItemComponent]
 })
 export class ProjectsModule { }
