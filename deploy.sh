767648e2d212431eda5e1567fdbe4a684ad605cd
GeorgeA93 <george.allen93@outlook.com>
2016-11-09 16:31:23 +0000

    updated travis and deploy profile

--- deploy.sh
+++ deploy.sh
@@ -11,5 +11,4 @@ cd dist
 git init
 git add .
 git commit -m "Deploy to Github Pages"
-# git push --force --quiet "https://${GH_TOKEN}@github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
-git push --force --quiet "https://a0e77ddc7d0c88311fed103db443bcd41a740dc3@github.com/GeorgeA93/grgaln.git" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
