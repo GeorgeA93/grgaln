683ecaf655b1828bacccaee1af241f25dfb84b84
GeorgeA93 <george.allen93@outlook.com>
2016-11-09 18:17:12 +0000

    Updated travis.yml

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 # See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
 
-if [[ $TRAVIS_BRANCH == 'master' ] && [$TRAVIS_PULL_REQUEST] != "false"]; then
-    set -o errexit
+if [[ $TRAVIS_BRANCH == 'master' ]]; then
+    if [[$TRAVIS_PULL_REQUEST]] != "false"]; then
+        set -o errexit
 
-    # config
-    git config --global user.email "george.allen93@outlook.com"
-    git config --global user.name "GeorgeA93"
+        # config
+        git config --global user.email "george.allen93@outlook.com"
+        git config --global user.name "GeorgeA93"
 
-    # deploy
-    cd dist
-    git init
-    git add .
-    git commit -m "Deploy to Github Pages"
-    git push --force --quiet "https://${GH_TOKEN}@github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
+        # deploy
+        cd dist
+        git init
+        git add .
+        git commit -m "Deploy to Github Pages"
+        git push --force --quiet "https://${GH_TOKEN}@github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
+    fi
 fi
