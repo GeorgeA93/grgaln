33a3a8dd00151fe838ffd2948103915c07b34333
GeorgeA93 <george.allen93@outlook.com>
2016-11-10 10:49:56 +0000

    implemented basic e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,10 +1,44 @@
 import { GrgalnPage } from './app.po';
+import { browser, element, by } from 'protractor';
 
 describe('grgaln App', function () {
   let page: GrgalnPage;
 
   beforeEach(() => {
     page = new GrgalnPage();
+    browser.ignoreSynchronization = true;
+  });
+
+  it('should create', () => {
+    expect(page).toBeTruthy();
+  });
+
+  it('should show home page with correct background image', () => {
+    browser.get('/');
+    let siteWrapper = element(by.css('.site-wrapper'));
+    let background = siteWrapper.getCssValue('background-image');
+    expect(background).toEqual('url("http://localhost:4200/assets/nightsky.jpg")');
+  });
+
+  it('should show home page title', () => {
+    browser.get('/');
+    let header = element(by.css('h1')).getText();
+    expect(header).toBeTruthy();
+  });
+
+  it('should navigate to projects', () => {
+    browser.get('/');
+    let button = element(by.css('a'));
+    button.click().then(() => {
+      expect(browser.getCurrentUrl()).toEqual("http://localhost:4200/projects");
+    });
+  });
+
+  it('should display some projects', () => {
+    browser.get('/projects');
+    browser.sleep(2000); // TODO is this the best way to do it??
+    let projects = element.all(by.css('app-project-item'));
+    expect(projects.count()).toBeGreaterThan(2);
   });
 
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,15 @@
 import { browser, element, by } from 'protractor';
 
 export class GrgalnPage {
-  navigateTo() {
-    return browser.get('/');
-  }
+    navigateTo() {
+        return browser.get('/');
+    }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
-  }
+    getBackgroundImage() {
+        return element(by.css('img'));
+    }
+
+    getParagraphText() {
+        return element(by.css('app-root h1')).getText();
+    }
 }
