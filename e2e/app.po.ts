830ead5609d1d50d1780f7bd6cb58b94e44d856c
GeorgeA93 <george.allen93@outlook.com>
2016-11-16 20:19:23 +0000

    updated packages, closes #15 and closes #16

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,15 +1,15 @@
 import { browser, element, by } from 'protractor';
 
 export class GrgalnPage {
-    navigateTo() {
-        return browser.get('/');
-    }
+  navigateTo() {
+    return browser.get('/');
+  }
 
-    getBackgroundImage() {
-        return element(by.css('img'));
-    }
+  getBackgroundImage() {
+    return element(by.css('img'));
+  }
 
-    getParagraphText() {
-        return element(by.css('app-root h1')).getText();
-    }
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
 }
